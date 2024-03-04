b346ea4099360f21d195e32c40575109192ecf15
Lukas Lingys <97232387+Gifxz@users.noreply.github.com>
2024-03-04 11:47:42 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<form method="post" action="send-sms-with-tele2.php">
+                <p class="card-text">
+                    <label><span lang="lt">Gavėjas</span><span lang="en">Recipient</span></label>
+                    <input class="form-control" type="text" name="numeris" id="numeris" placeholder="+370 600 00000" aria-label="default input">
+                        
+                    <label><span lang="lt">Žinutė ( Slaptažodis )</span><span lang="en">Message ( Password )</span></label>
+                    <input type="text" class="form-control" name="zinute" id="zinute" rows="1" placeholder="Jūsų tekstas">       
+                </p>
+                <button class="btn btn-primary"><span lang="lt">Siųsti žinutę</span><span lang="en">Send message</span></button>
+</form>
